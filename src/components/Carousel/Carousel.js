63b00632a11247624b5e584abdfddfcf5adaaf27
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-11-08 05:00:41 +0600

    'home-carousel-hero-added'

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,7 +4,34 @@ import { Link } from 'react-router-dom';
 const Carousel = () => {
     return (
         <div>
-            
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/TmBZDkT/img-2.jpg" alt='' className="w-full brightness-50" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-1/3 top-1/3  text-5xl text-slate-300 font-bold">
+                        <h1>Hello, <br /> Wellcome to <span className='text-info'>GoTrip!</span></h1>
+                    </div>
+                    <div className="absolute flex justify-between left-1/2 top-1/2">
+                        <Link to='/services' className='btn btn-info'>Get Start ❯</Link>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/ygfcP2G/img-1.jpg" alt='' className="w-full brightness-50" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-1/3 top-1/3  text-5xl text-slate-300 font-bold">
+                        <h1>Hello, <br /> Wellcome to <span className='text-info'>GoTrip!</span></h1>
+                    </div>
+                    <div className="absolute flex justify-between left-1/2 top-1/2">
+                        <Link to='/services' className='btn btn-info'>Get Start ❯</Link>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/HeroHome.js
+++ src/components/Home/HeroHome.js
@@ -4,7 +4,18 @@ import { Link } from 'react-router-dom';
 const HeroHome = () => {
     return (
         <div>
-            
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className='w-1/2'>
+                        <img src="https://placeimg.com/260/400/arch" alt='' className="max-w-sm rounded-lg shadow-2xl" />
+                    </div>
+                    <div>
+                        <h1 className="text-5xl font-bold">We had a great relations with our consumer!</h1>
+                        <p className="py-6">We are highly commited to our customer to give them our best service. And our 95% of customer are happy with our service. So take a trip with GOTRIP.</p>
+                        <Link className="btn btn-info">Get Started</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
